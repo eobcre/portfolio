39b7394f8c9a85584fdede805e4ee86da4a6241a
Emmy Manning <emswoo@EM.local>
2026-02-28 17:44:49 -0500

    refactor: code, new: added icons

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
 const Hero = () => {
   const cards = [
@@ -9,19 +10,23 @@ const Hero = () => {
   ];
 
   return (
-    <div className="flex flex-col items-center gap-6 px-8 py-10 h-screen">
+    <div className="flex flex-col items-center gap-6 font-custom px-8 py-10 h-screen">
       <div>
-        <p className="font-custom text-main text-9xl uppercase">Emmy Manning</p>
-        <p className="font-custom text-main text-2xl text-right">AI Cloud Engineer</p>
+        <p className="text-main text-9xl uppercase">Emmy Manning</p>
+        <div className="flex justify-end items-center gap-2">
+          <p className="text-main text-2xl">AI Cloud Engineer ー</p>
+          <Icon icon="foundation:social-linkedin" width="40" height="40" className="text-main cursor-pointer hover:opacity-70 transition-all duration-300 ease-out" />
+          <Icon icon="proicons:github" width="40" height="40" className="text-main cursor-pointer hover:opacity-70 transition-all duration-300 ease-out" />
+        </div>
       </div>
       <div className="grid grid-cols-4 gap-4 w-2/3 h-full">
         {cards.map((card, index) => (
           <Link key={card.id} to={card.path}>
-            <div key={index} className={`bg-cover bg-center shadow-2xl rounded cursor-pointer hover:opacity-70 h-full ${card.bg}`}>
+            <div key={index} className={`bg-cover bg-center shadow-2xl rounded cursor-pointer hover:opacity-70 transition-all duration-300 ease-out h-full ${card.bg}`}>
               <div className="px-4">
-                <p className="text-white text-9xl font-custom">{card.order}</p>
+                <p className="text-white text-9xl">{card.order}</p>
                 <p className="border border-white"></p>
-                <p className="text-white text-3xl font-custom pt-2">{card.title}</p>
+                <p className="text-white text-3xl pt-2">{card.title}</p>
               </div>
             </div>
           </Link>
